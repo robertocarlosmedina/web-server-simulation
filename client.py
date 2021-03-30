67009fc84564ccbb0e4538ebb7028e85c637621b
Roberto-04-dev <true4321king@gmail.com>
2021-03-30 17:21:50 -0100

    Simulating the telnet command to check server connections.

--- client.py
+++ client.py
@@ -2,11 +2,29 @@ from socket import *
 
 serverName = 'localhost'
 serverPort = 80
+
+def verifyCmd(cmd):
+    cmd = cmd.split(" ")
+    if len(cmd) < 3:
+        return None
+    else:
+        return cmd
+
 while True:
-    clientSocket = socket(AF_INET, SOCK_STREAM)
-    clientSocket.connect((serverName, serverPort))
-    sentence = input('Input lowercase sentence: ')
-    clientSocket.send(sentence.encode())
-    modifiedSentence = clientSocket.recv(1024)
-    print('From Server: ', modifiedSentence.decode())
-    clientSocket.close()
+    cmd = input("TCP/Teste -> ")
+    verfcmd = verifyCmd(cmd)
+    if verfcmd != None:
+        serverName = verfcmd[0]
+        serverPort = int(verfcmd[1])
+        aux = verfcmd[2]
+        sentence = f'http://{serverName}:{serverPort}/{aux}'
+
+        try:
+            clientSocket = socket(AF_INET, SOCK_STREAM)
+            clientSocket.connect((serverName, serverPort))
+            clientSocket.send(sentence.encode())
+            modifiedSentence = clientSocket.recv(1024)
+            print('From Server: ', modifiedSentence.decode())
+            clientSocket.close()
+        except OSError:
+            print("File not found")
--- webserve.py
+++ webserve.py
@@ -1,11 +1,10 @@
 from socket import *
 from threading import Thread
-import sys
 
 class th(Thread):
-    def __init__(self, connectionSocket, output):
+    def __init__(self, connectionSocket, f):
         Thread.__init__(self)
-        self.outputdata = output
+        self.outputdata = f.read()
         self.connectionSocket = connectionSocket
 
     def run(self):
@@ -13,7 +12,8 @@ class th(Thread):
         for i in range(0, len(self.outputdata)):
             self.connectionSocket.send(self.outputdata[i].encode())
         self.connectionSocket.send("\r\n".encode())
-        self.connectionSocket.close()
+        # self.connectionSocket.close()
+        
 
 class Get():
     def __init__(self, port):
@@ -24,40 +24,42 @@ class Get():
         self.serverSocket = socket(AF_INET, SOCK_STREAM)
         self.serverSocket.bind(("localhost", self.serverPort))
         self.serverSocket.listen(1)
+        print(f'Port {all_ports[port]}: already connected.\n')
         while True:
-            
-            print('The server is ready to receive')
+            print('The server is under state active.')
 
             connectionSocket, addr = self.serverSocket.accept()
             try:
                 message = connectionSocket.recv(1024).decode()
+                print(message)
                 filename = message.split()[1]
-                
                 f = open(filename[1:])
-                outputdata = f.read()
-                self.files.append(outputdata)
+                
+                self.files.append(f)
                 for file in self.files:
-                    thread = th(connectionSocket, outputdata)
+                    thread = th(connectionSocket, file)
                     thread.start()
-                # connectionSocket.send("HTTP/1.1 200 OK\r\n\r\n".encode())
-                # for i in range(0, len(outputdata)):
-                #     connectionSocket.send(outputdata[i].encode())
-                # connectionSocket.send("\r\n".encode())
-                # connectionSocket.close()
+                connectionSocket.send("HTTP/1.1 200 Ok\r\n\r\n".encode())
+                
             except IOError:
                 connectionSocket.send("HTTP/1.1 404 Not Found\r\n\r\n".encode())
                 connectionSocket.send("<html><head></head><body><h1>404 Not Found</h1></body></html>\r\n".encode())
                 connectionSocket.close()
 
 all_ports=[3000, 3001, 3002, 3003]
-get = Get(3000)
-get.run()
+port = 0
 
+while True:
+    stop = True
+    try:
+        print(f'Port {all_ports[port]}: trying to connect.')
+        get = Get(all_ports[port])
+        get.run()
+    except OSError:
+        stop = False
+        print(f'Port {all_ports[port]}: is unavailable now.')
 
-# serverSocket.close()  
-# sys.exit()
-            
-
-
-
-
+    if stop:
+        break
+    port += 1
+# get.run()       
